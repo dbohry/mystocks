c42245c9af75efbbed1770aca9b3f167d31e2ae0
Daniel Bohry <danielb@dbserver.com.br>
2017-11-03 15:14:53 -0200

    alignment

--- main.go
+++ main.go
@@ -9,14 +9,11 @@ import (
 )
 
 func main() {
-
 	router := mux.NewRouter()
+	router.HandleFunc("/stocks", controllers.GetStocks).Methods("GET")
+	router.HandleFunc("/stocks", controllers.SaveStock).Methods("POST")
+	router.HandleFunc("/stocks/{id}", controllers.GetStock).Methods("GET")
+	router.HandleFunc("/stocks/{id}", controllers.DeleteStock).Methods("DELETE")
 	
-		router.HandleFunc("/stocks", controllers.GetStocks).Methods("GET")
-		router.HandleFunc("/stocks", controllers.SaveStock).Methods("POST")
-		router.HandleFunc("/stocks/{id}", controllers.GetStock).Methods("GET")
-		router.HandleFunc("/stocks/{id}", controllers.DeleteStock).Methods("DELETE")
-		
 	log.Fatal(http.ListenAndServe(":8080", router))
-
 }
